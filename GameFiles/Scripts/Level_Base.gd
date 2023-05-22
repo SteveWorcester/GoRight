0d7e748cff0c35fc98a90db2874a9f25f4e95e54
Steve <worcester.steve@gmail.com>
2023-05-21 17:33:51 -0700

    implement random generation engine
    implement random generation of events.
    
    - event levels can be set per stage/level
    - event generation not in yet, this is just the framework to generate
    them.

--- GameFiles/Scripts/Level_Base.gd
+++ GameFiles/Scripts/Level_Base.gd
@@ -2,17 +2,22 @@ class_name Level_Base
 extends Node2D
 
 signal eventLevelsSetSuccess
+@onready var event = preload("res://GameFiles/Enums/EventLevel.gd")
 
-var eventLevels = {
-	"event_chance": null,
-	"enemy_chance": null,
-	"treasure_chance": null,
-	"distance_per_chance_roll": null,
-	"stage_length" : null
-}
+var eventLevels : Dictionary
 
 func _ready() -> void:
+#	create_event_levels_dictionary()
 	pass
 
 func setLevelsComplete():
+	#await get_tree().process_frame
+	var levelManager = get_parent()
+	levelManager.connect_stage_signal()
+	#await get_tree().process_frame
+	print("emitting signal to build level events...")
 	emit_signal("eventLevelsSetSuccess")
+
+#func create_event_levels_dictionary():
+#	for lvl in event.level:
+#		eventLevels[lvl] = null
--- Levels/Stage1/Level1/S1_L1.gd
+++ Levels/Stage1/Level1/S1_L1.gd
@@ -2,9 +2,11 @@ extends Level_Base
 
 func _ready() -> void:
 	await super._ready()
-	eventLevels["distance_per_chance_roll"] = 10
-	eventLevels["event_chance"] = 10
-	eventLevels["enemy_chance"] = 50
-	eventLevels["treasure_chance"] = 30
-	eventLevels["stage_length"] = 100
+	eventLevels[event.level.distance_per_chance_roll] = 10
+	eventLevels[event.level.event_chance] = 10
+	eventLevels[event.level.enemy_chance] = 50
+	eventLevels[event.level.treasure_chance] = 30
+	eventLevels[event.level.stage_length] = 100
+	eventLevels[event.level.none] = 10
+	print("event levels set for S1_L1...")
 	setLevelsComplete()
